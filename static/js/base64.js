df26bcc55daabca601579938dfb655e32e21af69
juls0730 <juliangarner240@gmail.com>
2022-05-21 18:50:46 -0500

    new blog

--- static/js/base64.js
+++
@@ -1,5 +0,0 @@
-const emailencode = document.getElementById('emailencoded')
-const encodedtex = emailencode.textContent
-const decodedtex = atob(encodedtex)
-
-emailencode.innerHTML = '<a class="socials-link" href=mailto:' + decodedtex + '>' + decodedtex + '</a>'
